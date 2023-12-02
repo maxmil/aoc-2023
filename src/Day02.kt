a117ffd69036dc49b8f57ecf3deff9ebf0e97813
Max Pimm <maxmil@gmail.com>
2023-12-02 14:05:57 +0530

    Day 2 after a bit of love
    No substantial changes. Quite happy with my first solution although nothing very special about it.
    
    Could have modified the parse method to keep only the max balls or min balls but doesn't gain enough.

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,47 +1,42 @@
 import kotlin.math.max
-import kotlin.math.min
 
 fun main() {
 
-    data class Pick(val blue:Int, val red:Int, val green:Int)
+    data class Pick(val blue:Int, val red:Int, val green:Int) {
+        fun max(other: Pick) = Pick(max(other.blue, blue), max(other.red, red), max(other.green, green))
+    }
     data class Game(val id:Int, val picks: List<Pick>)
 
-    fun parseGames(input: List<String>): List<Game> {
-        val games = input.map { line ->
-            line.split(":").let { s ->
-                val id = s[0].split(" ")[1].toInt()
-                val picks = s[1].split(";").map { pick ->
-                    pick.split(",").fold(Pick(0, 0, 0)) { acc, color ->
-                        color.trim().split(" ").let {
-                            if (it[1] == "blue") acc.copy(blue = acc.blue + it[0].toInt())
-                            else if (it[1] == "green") acc.copy(green = acc.green + it[0].toInt())
-                            else acc.copy(red = acc.red + it[0].toInt())
+    fun parseGames(input: List<String>): List<Game> = input.map { line ->
+        line.split(":").let { (game, colors) ->
+            val id = game.split(" ")[1].toInt()
+            val picks = colors.split(";").map { pick ->
+                pick.split(",").fold(Pick(0, 0, 0)) { acc, color ->
+                    color.trim().split(" ").let { (count, color) ->
+                        when (color) {
+                            "blue" -> acc.copy(blue = acc.blue + count.toInt())
+                            "green" -> acc.copy(green = acc.green + count.toInt())
+                            else -> acc.copy(red = acc.red + count.toInt())
                         }
                     }
                 }
-                Game(id, picks)
             }
+            Game(id, picks)
         }
-        return games
     }
 
-    fun part1(input: List<String>): Int {
-        return parseGames(input).filter { g -> g.picks.all { p -> p.red < 13 && p.green < 14 && p.blue < 15 } }
-            .sumOf { it.id }
+    fun part1(input: List<String>) = parseGames(input)
+        .filter { it.picks.all { p -> p.red < 13 && p.green < 14 && p.blue < 15 } }
+        .sumOf { it.id }
 
-    }
-
-    fun part2(input: List<String>): Int {
-        return parseGames(input).map{ game ->
-            game.picks.drop(1).fold(game.picks.first()) { acc, pick ->
-                Pick(max(acc.blue, pick.blue), max(acc.red, pick.red), max(acc.green, pick.green))
-            }.let { it.red * it.blue * it.green}
-        }.sum()
+    fun part2(input: List<String>) = parseGames(input).sumOf { game ->
+        val max = game.picks.drop(1).fold(game.picks.first()) { acc, pick -> acc.max(pick)}
+        max.red * max.blue * max.green
     }
 
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 8)
-    check(part2(testInput).also{ println(it) } == 2286)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
