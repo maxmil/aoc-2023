6afa8876619173eac3749b6a694363faf4ad1fd9
Max Pimm <maxmil@gmail.com>
2023-12-04 11:41:49 +0100

    Day 4 polished

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,20 +2,17 @@ import kotlin.math.pow
 import kotlin.time.measureTimedValue
 
 fun main() {
+    fun String.won() = substringAfter(": ").split(" | ")
+        .map { it.split(" +".toRegex()).toSet() }
+        .let { (winning, numbers) -> winning.intersect(numbers).size }
+
     fun part1(input: List<String>): Int {
-        return input.sumOf { line ->
-            val (wining, numbers) = line.substringAfter(":").split("|").map { it.trim().split(" +".toRegex()).toSet() }
-            wining.intersect(numbers).size.let { if (it == 0) 0 else 2.0.pow(it - 1).toInt() }
-        }
+        return input.sumOf { line -> line.won().let { if (it == 0) 0 else 2.0.pow(it - 1).toInt() } }
     }
 
     fun part2(input: List<String>): Int {
         val cards = Array(input.size) { 1 }
-        input.forEachIndexed { i, line ->
-            val (wining, numbers) = line.substringAfter(":").split("|").map { it.trim().split(" +".toRegex()).toSet() }
-            val won = wining.intersect(numbers).size
-            (i + 1..i + won).forEach { cards[it] += cards[i] }
-        }
+        input.forEachIndexed { i, line -> (i + 1..i + line.won()).forEach { cards[it] += cards[i] } }
         return cards.sum()
     }
 
