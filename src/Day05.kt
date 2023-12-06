f9f45b7d7821ba5e8f072ed33a6e2175d6c83839
Max Pimm <maxmil@gmail.com>
2023-12-06 03:25:28 +0100

    Day 5 using long ranges rather than data class

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,38 +1,39 @@
 import kotlin.time.measureTimedValue
 
 fun main() {
-    data class Range(val src: Long, val dest: Long, val range: Long)
 
-    fun String.parseMaps(): List<List<Range>> = split("\n\n").drop(1).map { m ->
-        m.split("\n")
-            .drop(1).map { range ->
-                range.split(" ").map { it.toLong() }.let { v -> Range(v[1], v[0], v[2]) }
+    fun String.parseMaps(): List<Map<LongRange, LongRange>> = split("\n\n").drop(1).map { m ->
+        m.split("\n").drop(1).associate { range ->
+            range.split(" ").map { it.toLong() }.let { (dest, src, range) ->
+                Pair(src..<src + range, dest..<dest + range)
             }
+        }
     }
 
     fun part1(input: String): Long {
         val seeds = input.substringBefore("\n").substringAfter(": ")
             .split(" ").map { it.toLong() }
         val maps = input.parseMaps()
-        return seeds.minOf { s -> maps.fold(s) { src, ranges ->
-                ranges.find { it.src <= src && it.src + it.range >= src }
-                    ?.let { it.dest + src - it.src }
-                    ?: src
+        return seeds.minOf { s ->
+            maps.fold(s) { v, map ->
+                map.entries.firstOrNull { v in it.key }
+                    ?.let { (src, dest) -> dest.first + v - src.first }
+                    ?: v
             }
         }
     }
 
     fun part2(input: String): Long {
         val seedRanges = input.substringBefore("\n").substringAfter(": ")
-            .split(" ").map { it.toLong() }.chunked(2).map { (a, b) -> a until a + b }
+            .split(" ").map { it.toLong() }.chunked(2).map { (a, b) -> a..<a + b }
         val maps = input.parseMaps().reversed()
 
         var location = 0L
         while (true) {
-            val seed = maps.fold(location) { dest, ranges ->
-                ranges.find { it.dest <= dest && it.dest + it.range > dest }
-                    ?.let { it.src + dest - it.dest }
-                    ?: dest
+            val seed = maps.fold(location) { v, map ->
+                map.entries.firstOrNull { v in it.value }
+                    ?.let { (src, dest) -> src.first + v - dest.first }
+                    ?: v
             }
             if (seedRanges.any { it.contains(seed) }) return location
             location++
@@ -44,6 +45,8 @@ fun main() {
     check(part2(testInput) == 46L)
 
     val input = readInputText("Day05")
+//    check(part1(input) == 51752125L)
+//    check(part2(input) == 12634632L)
     measureTimedValue { part1(input) }.also { println("${it.value} in ${it.duration}") }
     measureTimedValue { part2(input) }.also { println("${it.value} in ${it.duration}") }
 }
