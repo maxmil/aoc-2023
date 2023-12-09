476077ce53f3c014b86fe6e6f27b269633d0dc19
Max Pimm <maxmil@gmail.com>
2023-12-09 08:36:08 +0530

    Day 8 - Cleaned up
    This is a solution to the general case with offset cycles and multiple end nodes per cycle not the LCM which would have been easier to implement.
    
    This detects cycles and then uses brute force with an increment taken from the first cycle.

--- src/Day08.kt
+++ src/Day08.kt
@@ -12,71 +12,60 @@ fun main() {
         }
         return i
     }
+    data class Cycle(val start: Int, val size: Int, val endNodes: List<Int>)
 
-    data class NodePosition(val pos: Int, val node: String)
-    data class Cycle(val startNode: String, val start: Int, val end: Int, val endNodes: List<NodePosition>) {
-        fun size() = end - start
-    }
-
-    fun findSolution(cycles: MutableList<Cycle>):Long {
+    fun findCycleIntercept(cycles: List<Cycle>): Long {
         val cycle = cycles.first()
         val others = cycles.drop(1)
         var cnt = 0L
-        while(true) {
-            val pos = cnt * cycle.size() + cycle.start + cycle.endNodes[0].pos
-            if (cnt % 1000_000 == 0L) println("pos=${pos}")
-//            others.map { other -> other.endNodes.map {endNode ->
-//                println("${other.startNode} ${(pos - other.start - endNode.pos)} ${other.size()}")
-//            } }
-            if (others.all {other -> other.endNodes.any { (pos - other.start - it.pos) % other.size() == 0L } }){
-                return pos + 1
+        while (true) {
+            cycle.endNodes.forEach { endNode ->
+                val pos = cnt * cycle.size + endNode
+                if (others.all { other -> other.endNodes.any { (pos - it) % other.size == 0L } }) {
+                    return pos + 1
+                }
+                cnt++
             }
-            cnt ++
         }
+    }
+
+    fun detectCycles(allNodes: Map<String, Pair<String, String>>, dirs: String): Array<Cycle?> {
+        data class NodeAndMove(val pos: Int, val node: String)
+
+        var nodes = allNodes.keys.filter { it.endsWith('A') }
+        val seenNodes = Array<MutableMap<NodeAndMove, Int>>(nodes.size) { mutableMapOf() }
+        val cycles = Array<Cycle?>(nodes.size) { null }
+        var i = 0
+        while (cycles.any { it == null }) {
+            val moveIndex = i % dirs.length
+            nodes = nodes.map { if (dirs[moveIndex] == 'L') allNodes[it]!!.first else allNodes[it]!!.second }
+            nodes.forEachIndexed { index, node ->
+                if (cycles[index] == null) {
+                    val seenAt = seenNodes[index][NodeAndMove(moveIndex, node)]
+                    if (seenAt != null) {
+                        val endNodes = seenNodes[index].filter { it.key.node.endsWith('Z') }.map { it.key.pos }
+                        cycles[index] = Cycle(seenAt, i - seenAt, endNodes)
+                    } else {
+                        seenNodes[index][NodeAndMove(i, node)] = i
+                    }
+                }
+            }
+            i++
+        }
+        return cycles
     }
 
     fun part2(input: List<String>): Long {
         val dirs = input.first()
         val nodes = input.drop(2).associate { it.substring(0, 3) to (it.substring(7..9) to it.substring(12..14)) }
-        val firstSeenByStartNode = nodes.keys.filter { it.endsWith('A') }.associateWith { mutableMapOf<NodePosition, Int>() }
-        val startNodes = firstSeenByStartNode.keys.toList()
-        var currNodes = startNodes
-        val cycles = mutableListOf<Cycle>()
-        var i = 0
-        while (cycles.size < startNodes.size) {
-            val pos = i % dirs.length
-            currNodes = currNodes.map { if (dirs[pos] == 'L') nodes[it]!!.first else nodes[it]!!.second }
-            currNodes.forEachIndexed { nodeInd, node ->
-                val startNode = startNodes[nodeInd]
-                if (cycles.all { it.startNode != startNode }) {
-                    val seenNodes = firstSeenByStartNode[startNode]!!
-                    val seenAt = seenNodes[NodePosition(pos, node)]
-                    if (seenAt != null) {
-//                        println(seenNodes.entries.groupingBy { it.key.node }.eachCount())
-                        val endNodes = seenNodes.entries.filter { it.key.node.endsWith('Z') }
-                            .map { it.key.copy(pos = it.key.pos - seenAt) }
-                        cycles.add(Cycle(startNode, seenAt, i, endNodes))
-                    } else {
-                        seenNodes[NodePosition(i, node)] = i
-                    }
-                }
-            }
-            i ++
-        }
-
-        println(cycles)
-
-        return findSolution(cycles)
-//        return 0
+        val cycles = detectCycles(nodes, dirs)
+        return findCycleIntercept(cycles.filterNotNull())
     }
 
     check(part1(readInput("Day08_test")) == 6)
-//    check(part2(readInput("Day08_part2_test")).also { println(it) } == 6L)
+    check(part2(readInput("Day08_part2_test")) == 6L)
 
     val input = readInput("Day08")
     measureTimedValue { part1(input) }.also { println("${it.value} in ${it.duration}") }
     measureTimedValue { part2(input) }.also { println("${it.value} in ${it.duration}") }
-
-    // 217248968842 too low
-    // 5328603012082
 }
