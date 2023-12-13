c910698d5205d17ea7273fc20f55d354299a8b9c
Max Pimm <maxmil@gmail.com>
2023-12-13 08:03:36 +0530

    Day 12 - Part 2 unsolved

--- src/Day12.kt
+++ src/Day12.kt
@@ -2,50 +2,67 @@ import kotlin.time.measureTimedValue
 
 fun main() {
 
-    fun permutations(record: String, sizes: List<Int>, resolved: String = ""): Int {
-        var found = 0
+    val cache = mutableMapOf<Pair<String, List<Int>>, Long>()
+
+    var totalFound = 0
+
+    fun arrangements(record: String, sizes: List<Int>, resolved: String = ""): Long {
+        val key = Pair(record, sizes)
+        if (cache.containsKey(key)) return cache[key]!!
+        var found = 0L
         var i = 0
         val size = sizes.first()
         val rest = sizes.subList(1, sizes.size)
-        while (i <= record.length - size - rest.sum() - rest.size) {
+        while (i <= record.length - size - rest.sum() - rest.size
+//            && found < 1000_000_000L
+        ) {
             val candidate = record.substring(i, i + size)
             if (!candidate.contains('.')) {
                 if (rest.isEmpty()) {
                     if (!record.substring(i + size, record.length).contains('#')) {
 //                        val r = resolved + record.substring(0, i) + "#".repeat(size) + record.substring(i + size, record.length)
-//                        println("Found " + r.replace('?', '.'))
+//                        println("Found " + r.replace('?', ' '))
+//                        if (totalFound++ > 1000) System.exit(0)
                         found += 1
                     }
                 } else {
                     if (record[i + size] != '#') {
-                        val resolved1 = resolved + record.substring(0, i) + "#".repeat(size) + record[i + size + 1]
-                        found += permutations(record.substring(i + size + 1, record.length), rest, resolved1)
+                        val resolved1 = resolved + record.substring(0, i) + "#".repeat(size) + record[i + size]
+                        found += arrangements(record.substring(i + size + 1, record.length), rest, resolved1)
                     }
                 }
             }
             if (candidate.startsWith('#')) return found
             i++
         }
+        cache[key] = found
         return found
     }
 
-    fun part1(input: List<String>): Int {
-        return input.sumOf { line ->
-            val (record, sizesString) = line.split(" ")
-            val sizes = sizesString.split(",").map { it.toInt() }
-            permutations(record, sizes)
-        }
+    fun arrangements(line: String, repeat: Int): Long {
+        val (record, sizesString) = line.split(" ")
+        val sizes = sizesString.split(",").map { it.toInt() }
+        val recordRepeated = (Array(repeat) { record }).joinToString("?")
+        val sizesRepeated = (Array(repeat) { sizes }).flatMap { it }
+        return arrangements(recordRepeated, sizesRepeated)
+//            .also { println("$line $it") }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part1(input: List<String>): Int {
+        return input.sumOf { arrangements(it, 1).toInt() }
+    }
+
+    fun part2(input: List<String>): Long {
+//        return arrangements("?????????#?..??. 2,1,1,1", 5)
+        return input.sumOf { arrangements(it, 5) }
     }
 
     val testInput = readInput("Day12_test")
     check(part1(testInput) == 21)
-    // check(part2(testInput) == 1)
+    check(part2(testInput) == 525152L)
 
     val input = readInput("Day12")
-    measureTimedValue { part1(input) }.also { println("${it.value} in ${it.duration}") }
-    // measureTimedValue { part2(input) }.also { println("${it.value} in ${it.duration}")}
+    measureTimedValue { part1(input) }.also { check(it.value == 7047) }
+        .also { println("${it.value} in ${it.duration}") }
+//    measureTimedValue { part2(input) }.also { println("${it.value} in ${it.duration}")}
 }
