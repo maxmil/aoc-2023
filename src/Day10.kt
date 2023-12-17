7e1eb591031b7d9e29660b359f8d1163c19236ff
Max Pimm <maxmil@gmail.com>
2023-12-18 03:58:27 +0530

    Day 10 - Make elegant the default

--- src/Day10.kt
+++ src/Day10.kt
@@ -4,16 +4,6 @@ import kotlin.time.measureTimedValue
 fun main() {
 
     fun Cell.isInverse(cell: Cell) = x + cell.x == 0 && y + cell.y == 0
-    fun Cell.turn(clockwise: Boolean): Cell {
-        val m = if (clockwise) 1 else -1
-        return when (this) {
-            Cell(0, -1) -> Cell(m * 1, 0)
-            Cell(1, 0) -> Cell(0, m * 1)
-            Cell(0, 1) -> Cell(m * -1, 0)
-            Cell(-1, 0) -> Cell(0, m * -1)
-            else -> throw IllegalArgumentException()
-        }
-    }
 
     val pipes = mapOf(
         '|' to listOf(Cell(0, -1), Cell(0, 1)),
@@ -30,6 +20,7 @@ fun main() {
         while (true) {
             val curr = visited.last() + move
             if (curr == visited.first()) return visited
+            if (!grid.inBounds(curr)) return null
             val pipe = pipes[grid[curr]] ?: return null
             if (pipe.all { !it.isInverse(move) }) return null
             visited.add(curr)
@@ -44,47 +35,7 @@ fun main() {
             .size / 2
     }
 
-    fun isClockwiseLoop(moves: List<Cell>): Boolean {
-        return moves.windowed(3).fold(0) { acc, (a, b, c) ->
-            val prev = b - a
-            when (c - b) {
-                prev.turn(true) -> acc + 1
-                prev.turn(false) -> acc - 1
-                else -> acc
-            }
-        } > 0
-    }
-
-    fun inRegion(grid: Grid, start: Cell, edge: Collection<Cell>): Set<Cell> {
-        val toVisit = ArrayDeque(listOf(start))
-        val visited = edge.toMutableSet()
-        val found = mutableSetOf<Cell>()
-        while (toVisit.isNotEmpty()) {
-            val cell = toVisit.removeFirst()
-            if (!visited.contains(cell)) {
-                found.add(cell)
-                visited.add(cell)
-                toVisit.addAll(cell.adjacent().filter { grid.inBounds(it) })
-            }
-        }
-        return found
-    }
-
     fun part2(input: List<String>): Int {
-        val start = input.indexOfFirst { it.indexOf('S') > -1 }.let { Cell(input[it].indexOf('S'), it) }
-        val moves = listOf(Cell(0, 1), Cell(1, 0), Cell(0, -1), Cell(-1, 0))
-            .firstNotNullOf { moves(input, start, it) }
-        val clockwise = isClockwiseLoop(moves)
-        val inLoop = mutableSetOf<Cell>()
-        moves.windowed(3).forEach { (a, b, c) ->
-            listOf(b + (b - a).turn(clockwise), (b + (c - b).turn(clockwise)))
-                .filter { !moves.contains(it) && !inLoop.contains(it) }
-                .forEach { inLoop.addAll(inRegion(input, it, (moves + inLoop).toSet())) }
-        }
-        return inLoop.size
-    }
-
-    fun part2Elegant(input: List<String>): Int {
         val start = input.indexOfFirst { it.indexOf('S') > -1 }.let { Cell(input[it].indexOf('S'), it) }
         val moves = Cell(0, 0).adjacent().firstNotNullOf { moves(input, start, it) }
 
@@ -107,5 +58,4 @@ fun main() {
     val input = readInput("Day10")
     measureTimedValue { part1(input) }.also { println("${it.value} in ${it.duration}") }
     measureTimedValue { part2(input) }.also { println("${it.value} in ${it.duration}") }
-    measureTimedValue { part2Elegant(input) }.also { println("${it.value} in ${it.duration}") }
 }
