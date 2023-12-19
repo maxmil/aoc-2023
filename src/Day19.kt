b4fae340891db40000eb84aedfb2f6f79f70151e
Max Pimm <maxmil@gmail.com>
2023-12-19 17:48:03 +0530

    Day 19 - Part 2 cleaned up a bit

--- src/Day19.kt
+++ src/Day19.kt
@@ -48,9 +48,7 @@ fun main() {
 
     fun executeWorkflows(workflows: Map<String, Workflow>, part: Part): String {
         var wf = executeWorkflow(workflows["in"]!!, part)
-        while (wf != "A" && wf != "R") {
-            wf = executeWorkflow(workflows[wf]!!, part)
-        }
+        while (wf != "A" && wf != "R") wf = executeWorkflow(workflows[wf]!!, part)
         return wf
     }
 
@@ -59,24 +57,24 @@ fun main() {
         return parts.filter { executeWorkflows(workflows, it) == "A" }.sumOf { it.x + it.m + it.a + it.s }
     }
 
-    fun Rule.negate():Rule {
-        val comparator = if (comparator == '>') '<' else '>'
-        val target = if (this.comparator == '>') target + 1 else target - 1
+    fun Rule.negate(): Rule {
+        val (comparator, target) = if (comparator == '>') Pair('<', target + 1) else Pair('>', target - 1)
         return Rule(source, comparator, target, nextWorkflow)
     }
 
-    fun explodeWorkflow(workflows: Map<String, Workflow>, id: String): List<Pair<List<Rule>, String>> {
+    fun explodeRules(workflows: Map<String, Workflow>, id: String): List<Pair<List<Rule>, String>> {
         if (id == "A" || id == "R") return listOf(Pair(listOf(), id))
         val workflow = workflows[id]!!
         val negatedRules = workflow.rules.map { it.negate() }
         return (workflow.rules).flatMapIndexed { i, rule ->
-            val negated = negatedRules.subList(0, i)
-            explodeWorkflow(workflows, rule.nextWorkflow).map { Pair(negated + rule + it.first, it.second) }
-        } + explodeWorkflow(workflows, workflow.nextWorkflow).map { Pair(negatedRules + it.first, it.second)}
+            explodeRules(workflows, rule.nextWorkflow).map {
+                Pair(negatedRules.subList(0, i) + rule + it.first, it.second)
+            }
+        } + explodeRules(workflows, workflow.nextWorkflow).map { Pair(negatedRules + it.first, it.second) }
     }
 
-    fun boundsFor(rules: List<Rule>, category: Char) =
-        rules.fold(Pair(1, 4000)) { (minBound, maxBound), rule ->
+    fun List<Rule>.boundsFor(category: Char) =
+        fold(Pair(1, 4000)) { (minBound, maxBound), rule ->
             if (rule.source != category) Pair(minBound, maxBound)
             else when (rule.comparator) {
                 '>' -> Pair(max(minBound, rule.target + 1), maxBound)
@@ -84,38 +82,25 @@ fun main() {
             }
         }
 
-    fun rangeFor(rules:List<Rule>, category: Char): Long {
-        val bounds = boundsFor(rules, category)
+    fun List<Rule>.rangeFor(category: Char): Long {
+        val bounds = boundsFor(category)
         return (bounds.second - bounds.first + 1).toLong()
     }
 
     fun part2(input: String): Long {
         val workflows = parseWorkflows(input.split("\n\n")[0])
-        val exploded = explodeWorkflow(workflows, "in")
-
-//        println(exploded.size)
-//        exploded.forEach { println(it) }
-
-         val result = exploded.filter { (_, result) -> result == "A" }
-             .map { println(it);it }
-//            .map { (rules, _) -> listOf(boundsFor(rules, 'x'), boundsFor(rules, 'm'), boundsFor(rules, 'a'), boundsFor(rules, 's'))  }
-//             .forEach { println(it) }
-            .sumOf { (rules, _) -> rangeFor(rules, 'x') * rangeFor(rules, 'm') * rangeFor(rules, 'a') * rangeFor(rules, 's') }
-        return result
-//        return 0L
+        return explodeRules(workflows, "in")
+            .filter { (_, result) -> result == "A" }
+            .sumOf { (rules, _) ->
+                listOf('x', 'm', 'a', 's').fold(1L) { acc, category -> acc * rules.rangeFor(category) }
+            }
     }
 
     val testInput = readInputText("Day19_test")
     check(part1(testInput) == 19114)
-    check(part2(testInput).also { println(it) } == 167409079868000)
+    check(part2(testInput) == 167409079868000)
 
     val input = readInputText("Day19")
     measureTimedValue { part1(input) }.also { println("${it.value} in ${it.duration}") }
     measureTimedValue { part2(input) }.also { println("${it.value} in ${it.duration}") }
 }
-
-//167409079868000
-//167010937327821
-//166847596592385
-//166847596592385
-//167245503449662
