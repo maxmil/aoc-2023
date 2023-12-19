64e81e87b09bedd0455c2da898c70dfc746e8fc4
Max Pimm <maxmil@gmail.com>
2023-12-19 17:37:40 +0530

    Day 19 - Part 2 dirty

--- src/Day19.kt
+++ src/Day19.kt
@@ -1,3 +1,5 @@
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.time.measureTimedValue
 
 fun main() {
@@ -57,15 +59,63 @@ fun main() {
         return parts.filter { executeWorkflows(workflows, it) == "A" }.sumOf { it.x + it.m + it.a + it.s }
     }
 
+    fun Rule.negate():Rule {
+        val comparator = if (comparator == '>') '<' else '>'
+        val target = if (this.comparator == '>') target + 1 else target - 1
+        return Rule(source, comparator, target, nextWorkflow)
+    }
+
+    fun explodeWorkflow(workflows: Map<String, Workflow>, id: String): List<Pair<List<Rule>, String>> {
+        if (id == "A" || id == "R") return listOf(Pair(listOf(), id))
+        val workflow = workflows[id]!!
+        val negatedRules = workflow.rules.map { it.negate() }
+        return (workflow.rules).flatMapIndexed { i, rule ->
+            val negated = negatedRules.subList(0, i)
+            explodeWorkflow(workflows, rule.nextWorkflow).map { Pair(negated + rule + it.first, it.second) }
+        } + explodeWorkflow(workflows, workflow.nextWorkflow).map { Pair(negatedRules + it.first, it.second)}
+    }
+
+    fun boundsFor(rules: List<Rule>, category: Char) =
+        rules.fold(Pair(1, 4000)) { (minBound, maxBound), rule ->
+            if (rule.source != category) Pair(minBound, maxBound)
+            else when (rule.comparator) {
+                '>' -> Pair(max(minBound, rule.target + 1), maxBound)
+                else -> Pair(minBound, min(maxBound, rule.target - 1))
+            }
+        }
+
+    fun rangeFor(rules:List<Rule>, category: Char): Long {
+        val bounds = boundsFor(rules, category)
+        return (bounds.second - bounds.first + 1).toLong()
+    }
+
     fun part2(input: String): Long {
-        TODO()
+        val workflows = parseWorkflows(input.split("\n\n")[0])
+        val exploded = explodeWorkflow(workflows, "in")
+
+//        println(exploded.size)
+//        exploded.forEach { println(it) }
+
+         val result = exploded.filter { (_, result) -> result == "A" }
+             .map { println(it);it }
+//            .map { (rules, _) -> listOf(boundsFor(rules, 'x'), boundsFor(rules, 'm'), boundsFor(rules, 'a'), boundsFor(rules, 's'))  }
+//             .forEach { println(it) }
+            .sumOf { (rules, _) -> rangeFor(rules, 'x') * rangeFor(rules, 'm') * rangeFor(rules, 'a') * rangeFor(rules, 's') }
+        return result
+//        return 0L
     }
 
     val testInput = readInputText("Day19_test")
     check(part1(testInput) == 19114)
-    check(part2(testInput) == 167409079868000)
+    check(part2(testInput).also { println(it) } == 167409079868000)
 
     val input = readInputText("Day19")
     measureTimedValue { part1(input) }.also { println("${it.value} in ${it.duration}") }
     measureTimedValue { part2(input) }.also { println("${it.value} in ${it.duration}") }
 }
+
+//167409079868000
+//167010937327821
+//166847596592385
+//166847596592385
+//167245503449662
