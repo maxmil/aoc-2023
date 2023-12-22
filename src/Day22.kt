d54b7bf1824aa0c07f6f6287c29d1653f25df0dc
Max Pimm <maxmil@gmail.com>
2023-12-22 23:52:58 +0530

    Day 22 - Cleaned up a bit

--- src/Day22.kt
+++ src/Day22.kt
@@ -3,69 +3,60 @@ import kotlin.time.measureTimedValue
 
 fun main() {
 
-    data class Point(val x: Int, val y: Int, val z: Int) {
-        operator fun minus(point: Point) = Point(x - point.x, y - point.y, z - point.z)
-        operator fun plus(point: Point) = Point(x + point.x, y + point.y, z + point.z)
+    data class Point3D(val x: Int, val y: Int, val z: Int) {
+        operator fun minus(point: Point3D) = Point3D(x - point.x, y - point.y, z - point.z)
+        operator fun plus(point: Point3D) = Point3D(x + point.x, y + point.y, z + point.z)
+        operator fun div(n: Int) = Point3D(x / n, y / n, z / n)
+        operator fun times(n: Int) = Point3D(x * n, y * n, z * n)
     }
 
-    data class Brick(val id: Int, val start: Point, val end: Point) {
-        fun minZ() = start.z
-        fun points(z: Int): List<Point> {
-            if (end == start) return listOf(start.copy(z = z))
-            val diff = (end - start)
-            val dist = max(diff.x, max(diff.y, diff.z))
-            return (0..dist).map {
-                start.copy(z = z) + Point(diff.x * it / dist, diff.y * it / dist, diff.z * it / dist)
-            }
+    data class Brick(val location: Point3D, val size: Point3D) {
+        fun points(): List<Point3D> {
+            val dist = max(size.x, max(size.y, size.z))
+            val inc = if (dist > 0) (size / dist) else Point3D(0, 0, 0)
+            return (0..dist).map { location + inc * it }
         }
 
-        fun dropTo(z: Int): Brick {
-            val inc = z - this.start.z
-            return this.copy(start = start.copy(z = start.z + inc), end = end.copy(z = end.z + inc))
-        }
+        fun moveTo(z: Int) = this.copy(location = location.copy(z = z))
     }
 
-    fun processInput(input: List<String>): Pair<MutableMap<Brick, Set<Brick>>, List<Brick>> {
-        val bricks = input.mapIndexed { i, line ->
+    fun List<String>.dropBricks(): MutableMap<Brick, Set<Brick>> {
+        val initial = map { line ->
             line.split("~")
-                .map { coords -> coords.split(",").map { it.toInt() }.let { (x, y, z) -> Point(x, y, z) } }
-                .let { points -> points.sortedBy { it.z }.let { Brick(i, it[0], it[1]) } }
-        }.sortedBy { it.minZ() }
+                .map { coords -> coords.split(",").map { it.toInt() }.let { (x, y, z) -> Point3D(x, y, z) } }
+                .let { points -> points.let { Brick(it[0], it[1] - it[0]) } }
+        }.sortedBy { it.location.z }
 
-        val pile = mutableMapOf<Point, Brick>()
-        val supportedBy = mutableMapOf<Brick, Set<Brick>>()
-        val fallen = bricks.map { b ->
-            var z = b.minZ()
-            while (b.points(z - 1).all { !pile.contains(it) } && z > 1) z--
-            val moved = b.dropTo(z)
-            supportedBy[moved] = b.points(z - 1).mapNotNull { pile[it] }.toSet()
-            pile.putAll(b.points(z).associateWith { moved })
-            moved
+        val pile = mutableMapOf<Point3D, Brick>()
+        val bricks = mutableMapOf<Brick, Set<Brick>>()
+        initial.forEach { b ->
+            var z = b.location.z
+            while (b.moveTo(z - 1).points().all { !pile.contains(it) } && z > 1) z--
+            val moved = b.moveTo(z)
+            bricks[moved] = b.moveTo(z - 1).points().mapNotNull { pile[it] }.toSet()
+            pile.putAll(moved.points().associateWith { moved })
         }
-        return Pair(supportedBy, fallen)
+        return bricks
     }
 
     fun part1(input: List<String>): Int {
-        val (supportedBy, fallen) = processInput(input)
-        return fallen.filter { !supportedBy.values.contains(setOf(it)) }.size
+        val bricks = input.dropBricks()
+        return bricks.keys.filter { !bricks.values.contains(setOf(it)) }.size
     }
 
-    fun countFallen(supportedBy: Map<Brick, Set<Brick>>, brick: Brick): Int {
+    fun countFallen(bricks: Map<Brick, Set<Brick>>, brick: Brick): Int {
+        val canFall = bricks.filter { it.value.isNotEmpty() }
         val fallen = mutableSetOf(brick)
         while (true) {
-            val fall = supportedBy.filter {
-                !fallen.contains(it.key)
-                        && it.value.isNotEmpty()
-                        && it.value.all { b -> fallen.contains(b) }
-            }.keys
-            if (fall.isEmpty()) return fallen.count() - 1
-            fallen.addAll(fall)
+            val falling = canFall.filter { !fallen.contains(it.key) && it.value.all { b -> fallen.contains(b) } }.keys
+            if (falling.isEmpty()) return fallen.count() - 1
+            fallen.addAll(falling)
         }
     }
 
     fun part2(input: List<String>): Int {
-        val (supportedBy, fallen) = processInput(input)
-        return fallen.sumOf { countFallen(supportedBy, it) }
+        val bricks = input.dropBricks()
+        return bricks.keys.sumOf { countFallen(bricks, it) }
     }
 
     val testInput = readInput("Day22_test")
@@ -73,7 +64,6 @@ fun main() {
     check(part2(testInput) == 7)
 
     val input = readInput("Day22")
-    measureTimedValue { part1(input) }.also { check(it.value == 446) }.also { println("${it.value} in ${it.duration}") }
-    measureTimedValue { part2(input) }.also { check(it.value == 60287) }
-        .also { println("${it.value} in ${it.duration}") }
+    measureTimedValue { part1(input) }.also { println("${it.value} in ${it.duration}") }
+    measureTimedValue { part2(input) }.also { println("${it.value} in ${it.duration}") }
 }
